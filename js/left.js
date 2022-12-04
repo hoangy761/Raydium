f78b066a53e851d439d923fc00dd27bc4d9eb0fd
hoangy761 <hoangy761exe@gmail.com>
2022-12-05 01:41:56 +0700

    v3.3 js

---
+++ js/left.js
@@ -0,0 +1,22 @@
+function zoomInOut(){
+    var zoom = document.getElementById('spenrea_icon-left');
+    var names = document.getElementsByClassName('list_top_left_btn_title');
+    var boder_bottom =document.getElementById('spenrea_boder-bottom');
+    var show_icon = document.getElementById('show_icon');
+    if(zoom.title == 'show'){
+
+        for(var i =0; i< names.length; i++){
+            names[i].style.display = 'none'; 
+        }
+        boder_bottom.style.display = 'none';
+        show_icon.className='list_top_left_btn_icon fa-solid fa-right-to-bracket';
+        zoom.title = 'showless';
+    }else{
+        for(var i =0; i< names.length; i++){
+            names[i].style.display = 'block'; 
+        }
+        boder_bottom.style.display = 'block'; 
+        show_icon.className='list_top_left_btn_icon fa-sharp fa-solid fa-left-long'
+        zoom.title = 'show';
+    }
+}
---
+++ js/right_main-body_swap.js
@@ -0,0 +1,9 @@
+function swapIndex(){
+    var box_top = document.getElementById('box_top');
+    var box_bottom = document.getElementById('box_bottom');
+
+    // bien tam de swap index
+    var tmp = box_top.innerHTML;
+    box_top.innerHTML = box_bottom.innerHTML;
+    box_bottom.innerHTML = tmp;
+}
