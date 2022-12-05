dc6962e680e04879015b8c33acc07fdc90ceee74
hoangy761 <hoangy761exe@gmail.com>
2022-12-05 20:45:54 +0700

    v4.3 add chose token

---
+++ js/right_main-searchToken.js
@@ -0,0 +1,21 @@
+function chose_Coin(){
+    var right_main = document.getElementById('right_main_id');
+    var right_main_select = document.getElementById('right_main_select_id');
+    // right_main_select.style.display = 'block'
+    right_main.style.display='none';
+
+}
+
+function chose_Coin_Off(){
+    var right_main_select = document.getElementById('right_main_select_id');
+    var right_main = document.getElementById('right_main_id');
+    // right_main_select.style.display = 'none'
+    right_main.style.display='block';
+}
+
+var input_number_top = document.getElementById('input_top').value;
+var input_number_bottom = document.getElementById('input_bottom').value;
+
+if(input_number_top == 12){
+    alert('okela')
+}
